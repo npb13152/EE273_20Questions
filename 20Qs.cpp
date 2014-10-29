395744dbc4c55ac4f97772d9857e6ecf39b0cc06
npb13152 <npb13152@users.noreply.github.com>
2014-10-29 11:04:16 +0000

    Update 20Qs.cpp
    C++ Program

--- 20Qs.cpp
+++ 20Qs.cpp
@@ -3,9 +3,11 @@
 
 using namespace std;
 
+//set initial number and string variable
 int x=50;
 string input;
 
+//function decides what calculation to make based on user input
 int calcFunc()
 {
 	if(input=="y"||input=="yes")
@@ -21,6 +23,7 @@ int calcFunc()
 
 int main()
 {
+	//loop iterates until 20 questions have been asked
 	for(int counter=0; counter <20; counter++)
 	{
 		cout <<"Is your number less than " <<x <<"?: ";
@@ -28,8 +31,9 @@ int main()
 		calcFunc();
 		cout << x <<endl;
 	}
+	//"guessed" number printed
 	cout <<"Your number is " <<x <<endl;
-
 	system("PAUSE");
+	
 	return 0;
 }
